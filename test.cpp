a64e3e8f1cf67a28133f02c3963d11bdca18315e
Steve Herrick <s_herrick2@verizon.net>
2018-07-29 16:17:53 -0400

    updated

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 
 int main
 {
-    std::cout << "Hello Steve" << std::endl;
+    std::cout << "Hello Jane" << std::endl;
     std::cout << "World" << std::endl;
     std::cout << "World 1" << std::endl;
 }
